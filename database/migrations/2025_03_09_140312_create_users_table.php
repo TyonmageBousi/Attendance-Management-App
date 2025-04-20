cb176a68750ad5dfb4b934929388b921eea38b3b
ちょんまげ <qcomekei@gmail.com>
2025-04-20 23:54:21 +0900

    すみませんでした

--- database/migrations/2025_03_09_140312_create_users_table.php
+++ database/migrations/2025_03_09_140312_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('id');
+            $table->id('id'); //絶対idにして
             $table->string('name');
             $table->string('email');
             $table->string('password')->unique();;
